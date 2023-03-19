401dc7b3664196cda2223128fd0783e6c084a760
Aria Stewart <aredridel@dinhe.net>
2023-03-19 17:05:31 -0400

    start

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<slot></slot>
+
+<style>
+	@import 'leaflet/dist/leaflet.css';
+	:global(body,html) {height: 100%; margin: 0}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import {LeafletMap, TileLayer} from 'svelte-leafletjs';
+
+    const mapOptions = {
+        center: [41.8240, -71.4128],
+        zoom: 11,
+    };
+    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const tileLayerOptions = {
+        minZoom: 0,
+        maxZoom: 20,
+        maxNativeZoom: 19,
+        attribution: "© OpenStreetMap contributors",
+    };
+
+    let leafletMap;
+</script>
+
+<div class="example">
+    <LeafletMap bind:this={leafletMap} options={mapOptions}>
+        <TileLayer url={tileUrl} options={tileLayerOptions}/>
+    </LeafletMap>
+</div>
+
+<style>
+  .example {
+    height: 100%;
+  }
+</style>
